2af5b32862f5ff9aa3310c7817e0b0db38c5d1b5
brettmadrid <brettmadrid9@gmail.com>
2022-02-14 21:23:28 -0800

    Delete task functionality added

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 
 export class Todo extends Component {
+  deleteHandler = e => {
+    this.props.deleteTask(this.props.id);
+  };
   render() {
     return (
       <div>
         <button>
           <i className='fas fa-pen' />
-          {/* Edit */}
         </button>
-        <button>
+        <button onClick={this.deleteHandler}>
           <i className='fas fa-trash' />
         </button>
         <li>{this.props.task}</li>
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Todo from './Todo';
 import TodoForm from './TodoForm';
+import { v4 as uuidv4 } from 'uuid';
 
 export class TodoList extends Component {
   constructor(props) {
@@ -22,14 +23,20 @@ export class TodoList extends Component {
     };
   }
 
+  deleteTask = id => {
+    this.setState({ todos: this.state.todos.filter(t => t.id !== id) });
+  };
+
   addTask = t => {
     console.log('In addTask');
     console.log(t);
-    const newTask = { task: t.task, completed: false, id: 3 };
+    const newTask = { task: t.task, completed: false, id: uuidv4() };
     this.setState({ todos: [...this.state.todos, newTask] });
   };
   render() {
-    const todos = this.state.todos.map(t => <Todo task={t.task} key={t.id} />);
+    const todos = this.state.todos.map(t => (
+      <Todo task={t.task} key={t.id} id={t.id} deleteTask={this.deleteTask} />
+    ));
     return (
       <div>
         <h1>Todo List</h1>
