221a2b733e196c532333c327808a5f846d5401ec
brettmadrid <brettmadrid9@gmail.com>
2022-02-17 16:39:54 -0800

    conditional render of edited task

--- src/Todo.js
+++ src/Todo.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import { EditTodoForm } from './EditTodoForm';
 
 export class Todo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isEditing: false,
+    };
+  }
+
   deleteHandler = e => {
     this.props.deleteTask(this.props.id);
   };
+
+  editHandler = e => {
+    this.setState({ isEditing: !this.state.isEditing });
+  };
+
   render() {
-    return (
+    let result = this.state.isEditing ? (
+      <EditTodoForm task={this.props.task} />
+    ) : (
       <div>
-        <button>
+        <button onClick={this.editHandler}>
           <i className='fas fa-pen' />
         </button>
         <button onClick={this.deleteHandler}>
@@ -16,6 +32,7 @@ export class Todo extends Component {
         <li>{this.props.task}</li>
       </div>
     );
+    return result;
   }
 }
 
